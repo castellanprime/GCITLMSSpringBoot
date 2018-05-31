0fdea014905dc7f2ac112b4b58b308262dbeb69f
iratusmachina <iratusmachina@DESKTOP-8OQ93TH.(none)>
2018-05-31 16:49:22 -0400

    Added code for the admin view

--- src/main/resources/static/src/app.js
+++ src/main/resources/static/src/app.js
@@ -5,5 +5,13 @@ lmsApp.config(function($routeProvider) {
         redirectTo: "/home"
     }).when("/home", {
     	templateUrl: "views/home.html"
+    }).when("/admin", {
+    	templateUrl: "views/admin/index.html"
+    }).when("/admin/publishers", {
+    	templateUrl: "views/admin/publishsers.html"
+    }).when("/librarian", {
+    	templateUrl: "views/librarian/index.html"
+    }).when("/borrower", {
+    	templateUrl: "views/borrower/index.html"
     })
 });
