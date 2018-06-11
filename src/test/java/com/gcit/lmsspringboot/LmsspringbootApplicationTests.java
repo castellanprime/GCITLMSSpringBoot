5e5d7fd8757db51a0024313845eae0c6a2f74be4
iratusmachina <iratusmachina@DESKTOP-8OQ93TH.(none)>
2018-06-11 15:27:15 -0400

    Removed test fixtures.

--- src/test/java/com/gcit/lmsspringboot/LmsspringbootApplicationTests.java
+++ src/test/java/com/gcit/lmsspringboot/LmsspringbootApplicationTests.java
@@ -19,39 +19,8 @@ import com.gcit.lmsspringboot.service.AdminService;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class LmsspringbootApplicationTests {
-	/**
+	
 	@Test
 	public void contextLoads() {
-	}**/
-	
-	@Autowired
-	LibraryBranchDAO libraryBranchDao;
-	
-	@Autowired
-	LibraryBranch branch;
-	
-	@Autowired
-	AdminService adminService;
-	
-	@Autowired
-	AuthorDAO authorDAO;
-	
-	@Test
-	public void testAuthorInsert() throws SQLException, ClassNotFoundException{
-		Author author = new Author();
-		author.setAuthorName("Gavin Crowder");
-		int authorId = authorDAO.addAuthorWithID(author);
-		System.out.println(authorId);
-		assertNotNull(authorId);
-	}
-	
-	@Test
-	public void testLibraryBranchInsert() throws SQLException, ClassNotFoundException{
-		LibraryBranch branch = new LibraryBranch();
-		branch.setBranchName("University Library");
-		branch.setBranchAddress("Cleveland, Ohio");
-		int branchId = libraryBranchDao.addBranchWithID(branch);
-		System.out.println(branchId);
-		assertNotNull(branchId);
 	}
 }
